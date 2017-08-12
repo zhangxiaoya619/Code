360986be157ad4fca27a9da4b25e518d9fb15aa0
张晓亚 <133110205@qq.com>
2017-08-12 16:33:19 +0800

    1

---
+++ AuditPracticalOperation/UI/KJRJ/KJRJ/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+//
+// SketchFlow 需要知道哪个控件程序集包含其屏幕。这是在创建项目时
+// 自动设置的，但是如果您手动更改控件程序集的名称，则也必须
+// 在此处手动更新它。
+//
+[assembly: Microsoft.Expression.Prototyping.Services.SketchFlowLibraries("KJRJ.Screens")]
+
+namespace KJRJ
+{
+	/// <summary>
+	/// App.xaml 的交互逻辑
+	/// </summary>
+	public partial class App : Application
+	{
+		public App()
+		{
+			this.Startup += this.App_Startup;
+		}
+
+		private void App_Startup(object sender, StartupEventArgs e)
+		{
+			this.StartupUri = new Uri(@"pack://application:,,,/Microsoft.Expression.Prototyping.Runtime;Component/WPF/Workspace/PlayerWindow.xaml");
+		}
+	}
+}
---
+++ AuditPracticalOperation/UI/KJRJ/KJRJ/KJRJScreens/xuanzekemu_的副本.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KJRJScreens
+{
+	/// <summary>
+	/// xuanzekemu_的副本.xaml 的交互逻辑
+	/// </summary>
+	public partial class xuanzekemu_的副本 : UserControl
+	{
+		public xuanzekemu_的副本()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
