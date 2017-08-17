6a1891e3e3ea0cf193c9471a6a86a2a94897b324
张晓亚 <133110205@qq.com>
2017-08-17 23:47:25 +0800

    11

--- AuditPracticalOperation/ViewModel/IPower.cs
+++ AuditPracticalOperation/ViewModel/IPower.cs
@@ -5,8 +5,16 @@ using System.Text;
 
 namespace ViewModel
 {
+    /// <summary>
+    /// 购买权限接口
+    /// </summary>
     public interface IPower
     {
+        /// <summary>
+        /// 获取用户是否已经购买激活
+        /// </summary>
+        /// <param name="practicalItemID">科目ID</param>
+        /// <returns>是否购买激活</returns>
         bool HasPower(string practicalItemID);
     }
 }
