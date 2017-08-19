a886c70d1cab0824e4ceea88dab91ba8df075429
lzk <aaa>
2017-08-19 15:08:12 +0800

    1111

---
+++ AuditPracticalOperation/AuditPracticalOperation/UContorls/UCMenu.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AuditPracticalOperation.UContorls
+{
+    /// <summary>
+    /// UCMenu.xaml 的交互逻辑
+    /// </summary>
+    public partial class UCMenu : UserControl
+    {
+        public UCMenu()
+        {
+            InitializeComponent();
+            this.Loaded += UCMenu_Loaded;
+        }
+
+        void UCMenu_Loaded(object sender, RoutedEventArgs e)
+        {
+            DefaultMenu.IsChecked = true;
+        }
+        public event UserLogoutHandler OnUserLogout;
+        public event UserClickMenuHandler OnUserClickMenu;
+
+        /// <summary>
+        /// 退出登录
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonLogout_Click(object sender, RoutedEventArgs e)
+        {
+            if (OnUserLogout != null)
+                OnUserLogout();
+        }
+
+        /// <summary>
+        /// 点击菜单
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UserChooseMenu(object sender, RoutedEventArgs e)
+        {
+            if (OnUserClickMenu != null)
+            {
+                OnUserClickMenu(int.Parse((sender as RadioButton).Tag.ToString()));
+            }
+        }
+    }
+    public delegate void UserLogoutHandler();
+    public delegate void UserClickMenuHandler(int index);
+}
---
+++ AuditPracticalOperation/ViewModel/ProofItem.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace ViewModel
+{
+    public class ProofItem : ViewModelBase
+    {
+        private string name;
+        private string path;
+        private FileTypeEnum type;
+        private ObservableCollection<ProofItem> proofs;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    RaisePropertyChanged("Name");
+                }
+            }
+        }
+        public string Path
+        {
+            get
+            {
+                return path;
+            }
+            set
+            {
+                if (path != value)
+                {
+                    path = value;
+                    RaisePropertyChanged("Path");
+                }
+            }
+        }
+        public FileTypeEnum Type
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                if (type != value)
+                {
+                    type = value;
+                    RaisePropertyChanged("Type");
+                }
+            }
+        }
+        public ObservableCollection<ProofItem> Proofs
+        {
+            get
+            {
+                return proofs;
+            }
+            set
+            {
+                if (proofs != value)
+                {
+                    proofs = value;
+                    RaisePropertyChanged("Proofs");
+                }
+            }
+        }
+        public ProofItem()
+        {
+            proofs = new ObservableCollection<ProofItem>();
+        }
+    }
+    public enum FileTypeEnum
+    {
+        Directory,
+        Word,
+        Excel,
+        Pdf,
+        Img,
+    }
+}
