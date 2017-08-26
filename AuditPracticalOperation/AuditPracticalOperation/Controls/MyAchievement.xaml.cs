a88e79fea4486589e47c8f63b9be9a18d8ba4fa8
lzk <aaa>
2017-08-26 22:30:15 +0800

    213

---
+++ AuditPracticalOperation/AuditPracticalOperation/Controls/MyAchievement.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Business;
+using Business.Processer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AuditPracticalOperation.Controls
+{
+    /// <summary>
+    /// MyAchievement.xaml 的交互逻辑
+    /// </summary>
+    public partial class MyAchievement : UserControl
+    {
+        public MyAchievement()
+        {
+            InitializeComponent();
+            this.Loaded += MyAchievement_Loaded;
+        }
+
+        void MyAchievement_Loaded(object sender, RoutedEventArgs e)
+        {
+            TextBlock_UserName.Text = SingletonManager.Get<UserProcesser>().GetUser().Name;
+            TextBlock_UserID.Text = SingletonManager.Get<UserProcesser>().GetUser().ID;
+            TextBlock_ranking.Text = SingletonManager.Get<UserProcesser>().GetUser().Name;
+        }
+    }
+}
