c4fee58d97addcb10d3cb734ad7145e24deba654
lzk <aaa>
2017-08-30 18:30:48 +0800

    lzkImageScale

--- AuditPracticalOperation/AuditPracticalOperation/Controls/ProofShow.xaml.cs
+++ AuditPracticalOperation/AuditPracticalOperation/Controls/ProofShow.xaml.cs
@@ -31,6 +31,12 @@ namespace AuditPracticalOperation.Controls
         {
             InitializeComponent();
             this.Loaded += ProofShow_Loaded;
+            imageView.OnClose += imageView_OnClose;
+        }
+
+        void imageView_OnClose()
+        {
+            GridImageShow.Visibility = System.Windows.Visibility.Collapsed;
         }
 
         private void ProofShow_Loaded(object sender, RoutedEventArgs e)
@@ -85,17 +91,13 @@ namespace AuditPracticalOperation.Controls
                         break;
                     case ViewModel.FileTypeEnum.Img:
                         string fileName = choiseItem.Path;
-                        BitmapImage image = new BitmapImage(new Uri(fileName));
-                        CurrentImage.Source = image;
                         GridImageShow.Visibility = System.Windows.Visibility.Visible;
-                        imageTitle.Text = choiseItem.Name;
+                        imageView.LoadImage(fileName);
                         break;
                     default:
                         break;
                 }
             }
-            originalX = tlt.X;
-            originalY = tlt.Y;
         }
 
         private void ChoiseDir_Checked(object sender, RoutedEventArgs e)
@@ -115,100 +117,6 @@ namespace AuditPracticalOperation.Controls
         private void ImageShow_Click(object sender, RoutedEventArgs e)
         {
             GridImageShow.Visibility = System.Windows.Visibility.Collapsed;
-        }
-
-
-
-
-
-        private bool isMouseLeftButtonDown = false;
-        Point previousMousePoint = new Point(0, 0);
-        double originalX = 0, originalY = 0;
-        private void CurrentImage_MouseWheel(object sender, MouseWheelEventArgs e)
-        {
-            //image缩放
-            Point centerPoint = e.GetPosition(CurrentImage);
-            Image image = sender as Image;
-            Point point = e.GetPosition(image);
-            double scale = e.Delta * 0.001;
-            ZoomImage(point, scale);
-        }
-        private void ZoomImage(Point point, double scale)
-        {
-            TransformGroup group = ImageTransformGroup;
-
-            Point pointToContent = group.Inverse.Transform(point);
-
-            ScaleTransform transform = group.Children[0] as ScaleTransform;
-
-            if (transform.ScaleX + scale <= 1)
-            {
-                transform.ScaleX = 1;
-            }
-            if (transform.ScaleY + scale <= 1)
-            {
-                transform.ScaleY = 1;
-            }
-            if (sfr.ScaleX <= 1 && sfr.ScaleY <= 1 && scale < 0)
-            {
-                tlt.X = originalX;
-                tlt.Y = originalY;
-                return;
-            }
-            transform.ScaleX += scale;
-
-            transform.ScaleY += scale;
-
-            TranslateTransform transform1 = group.Children[1] as TranslateTransform;
-
-            transform1.X = -1 * ((pointToContent.X * transform.ScaleX) - point.X);
-
-            transform1.Y = -1 * ((pointToContent.Y * transform.ScaleY) - point.Y);
-
-        }
-        private void img_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            isMouseLeftButtonDown = true;
-            previousMousePoint = e.GetPosition(CurrentImage);
-        }
-
-        private void img_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            isMouseLeftButtonDown = false;
-            if (sfr.ScaleX <= 1)
-            {
-                tlt.X = originalX;
-                tlt.Y = originalY;
-            }
-        }
-
-        private void img_MouseLeave(object sender, MouseEventArgs e)
-        {
-            isMouseLeftButtonDown = false;
-            if (sfr.ScaleX <= 1)
-            {
-                tlt.X = originalX;
-                tlt.Y = originalY;
-            }
-        }
-
-        private void img_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (isMouseLeftButtonDown == true)
-            {
-                Point position = e.GetPosition(CurrentImage);
-                tlt.X += position.X - this.previousMousePoint.X;
-                tlt.Y += position.Y - this.previousMousePoint.Y;
-            }
-        }
-
-        private void RotateTransform_Click(object sender, RoutedEventArgs e)
-        {
-            rotate.Angle += 90;
-            sfr.ScaleX = 1;
-            sfr.ScaleY = 1;
-            tlt.X = originalX;
-            tlt.Y = originalY;
         }
 
         private void pdfShow_Click(object sender, RoutedEventArgs e)
---
+++ AuditPracticalOperation/AuditPracticalOperation/UContorls/UCImageView.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AuditPracticalOperation.UContorls
+{
+    /// <summary>
+    /// UCImageView.xaml 的交互逻辑
+    /// </summary>
+    public partial class UCImageView : UserControl
+    {
+        public UCImageView()
+        {
+            InitializeComponent();
+        }
+        public event UserClickHandler OnClose;
+        public void LoadImage(string path)
+        {
+            BitmapImage image = new BitmapImage(new Uri(path));
+            img.Source = image;
+        }
+        Point dragStart;
+        int imageAngle;
+        // <summary>
+        /// 图片鼠标按下事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            dragStart = e.GetPosition(root);
+        }
+        /// <summary>
+        /// 图片鼠标滚轮滚动事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Image_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var mosePos = e.GetPosition(img);
+
+
+            var scale = transform.ScaleX * (e.Delta > 0 ? 1.2 : 1 / 1.2);
+            scale = Math.Max(scale, 1);
+
+
+            transform.ScaleX = scale;
+            transform.ScaleY = scale;
+
+
+            if (scale == 1)        //缩放率为1的时候，复位
+            {
+                translate.X = 0;
+                translate.Y = 0;
+            }
+            else                //保持鼠标相对图片位置不变
+            {
+                var newPos = e.GetPosition(img);
+
+
+                translate.X += (newPos.X - mosePos.X);
+                translate.Y += (newPos.Y - mosePos.Y);
+            }
+        }
+        /// <summary>
+        /// 图片鼠标移动事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Image_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Pressed)
+            {
+                return;
+            }
+
+
+            var current = e.GetPosition(root);
+            //将坐标都转换成image坐标系下的坐标
+            Point point0 = root.TranslatePoint(current, (UIElement)img);
+            Point point1 = root.TranslatePoint(dragStart, (UIElement)img);
+            translate.X += (point0.X - point1.X);
+            translate.Y += (point0.Y - point1.Y);
+
+
+            dragStart = current;
+        }
+        /// <summary>
+        /// 图片左转事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            imageAngle = (imageAngle + 90) % 360;
+            rotate.Angle = imageAngle;
+        }
+        /// <summary>
+        /// 图片右转事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            imageAngle = (imageAngle - 90) % 360;
+            rotate.Angle = imageAngle;
+        }
+
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (OnClose != null)
+                OnClose();
+        }
+    }
+}
