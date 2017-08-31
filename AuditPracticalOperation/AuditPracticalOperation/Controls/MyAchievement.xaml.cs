b3fd5867a0b88e3e66b4280be4aab9eb0546132d
lzk <aaa>
2017-08-31 20:46:00 +0800

    lzk_ranking

--- AuditPracticalOperation/AuditPracticalOperation/Controls/MyAchievement.xaml.cs
+++ AuditPracticalOperation/AuditPracticalOperation/Controls/MyAchievement.xaml.cs
@@ -2,6 +2,7 @@
 using Business.Processer;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -13,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ViewModel;
 
 namespace AuditPracticalOperation.Controls
 {
@@ -21,6 +23,7 @@ namespace AuditPracticalOperation.Controls
     /// </summary>
     public partial class MyAchievement : UserControl
     {
+        private ObservableCollection<RankingItem> obsRankings;
         public MyAchievement()
         {
             InitializeComponent();
@@ -29,11 +32,127 @@ namespace AuditPracticalOperation.Controls
 
         void MyAchievement_Loaded(object sender, RoutedEventArgs e)
         {
+            obsRankings = new ObservableCollection<RankingItem>();
             TextBlock_UserName.Text = SingletonManager.Get<UserProcesser>().GetUser().Name;
             TextBlock_UserID.Text = SingletonManager.Get<UserProcesser>().GetUser().ID;
-            TextBlock_ranking.Text = SingletonManager.Get<UserProcesser>().GetUser().Name;
             TextBlock_DoneCount.Text = SingletonManager.Get<PracticalItemProcesser>().GetHasDonePracticalItems().Count.ToString();
-            TextBlock_DonePercent.Text = ((int)SingletonManager.Get<PracticalItemProcesser>().GetHasDonePracticalItems().Count / SingletonManager.Get<PracticalItemProcesser>().GetPracticalItems().Count * 100).ToString();
+            int this_score = (int)SingletonManager.Get<PracticalItemProcesser>().GetHasDonePracticalItems().Count / SingletonManager.Get<PracticalItemProcesser>().GetPracticalItems().Count * 100;
+            TextBlock_DonePercent.Text = this_score.ToString();
+
+            int index = 0;
+            if (this_score == 100)
+            {
+                index = 0;
+            }
+            else if (this_score >= 90)
+            {
+                index = 1;
+            }
+            else if (this_score >= 80)
+            {
+                index = 2;
+            }
+            else if (this_score >= 70)
+            {
+                index = 3;
+            }
+            else if (this_score >= 50)
+            {
+                index = 4;
+            }
+            else
+            {
+                index = 5;
+            }
+
+            RankingItem[] items = new RankingItem[6];
+            RankingItem[] fixedItems = new RankingItem[5];
+            items[0] = new RankingItem()
+            {
+                Score = 100,
+                User_Name = "陈昊",
+                ImgSource = AppDomain.CurrentDomain.BaseDirectory + "Images\\touxiang3.png",
+            };
+            items[1] = new RankingItem()
+            {
+                Score = 90,
+                User_Name = "冯轲",
+                ImgSource = AppDomain.CurrentDomain.BaseDirectory + "Images\\touxiang2.png",
+            };
+            items[2] = new RankingItem()
+            {
+                Score = 80,
+                User_Name = "邓润如",
+                ImgSource = AppDomain.CurrentDomain.BaseDirectory + "Images\\touxiang3.png",
+            };
+            items[3] = new RankingItem()
+            {
+                Score = 70,
+                User_Name = "赵旭",
+                ImgSource = AppDomain.CurrentDomain.BaseDirectory + "Images\\touxiang2.png",
+            };
+            items[4] = new RankingItem()
+            {
+                Score = 50,
+                User_Name = "甘仁",
+                ImgSource = AppDomain.CurrentDomain.BaseDirectory + "Images\\touxiang2.png",
+            };
+            items[5] = new RankingItem()
+            {
+                Score = this_score,
+                User_Name = SingletonManager.Get<UserProcesser>().GetUser().Name,
+                ImgSource = AppDomain.CurrentDomain.BaseDirectory + "Images\\touxiang1.png",
+            };
+            switch (index)
+            {
+                case 0:
+                    obsRankings.Add(items[5]);
+                    obsRankings.Add(items[0]);
+                    obsRankings.Add(items[1]);
+                    obsRankings.Add(items[2]);
+                    obsRankings.Add(items[3]);
+                    break;
+                case 1:
+                    obsRankings.Add(items[0]);
+                    obsRankings.Add(items[5]);
+                    obsRankings.Add(items[1]);
+                    obsRankings.Add(items[2]);
+                    obsRankings.Add(items[3]);
+                    break;
+                case 2:
+                    obsRankings.Add(items[0]);
+                    obsRankings.Add(items[1]);
+                    obsRankings.Add(items[5]);
+                    obsRankings.Add(items[2]);
+                    obsRankings.Add(items[3]);
+                    break;
+                case 3:
+                    obsRankings.Add(items[0]);
+                    obsRankings.Add(items[1]);
+                    obsRankings.Add(items[2]);
+                    obsRankings.Add(items[5]);
+                    obsRankings.Add(items[3]);
+                    break;
+                case 4:
+                    obsRankings.Add(items[0]);
+                    obsRankings.Add(items[1]);
+                    obsRankings.Add(items[2]);
+                    obsRankings.Add(items[3]);
+                    obsRankings.Add(items[5]);
+                    break;
+                default:
+                    obsRankings.Add(items[0]);
+                    obsRankings.Add(items[1]);
+                    obsRankings.Add(items[2]);
+                    obsRankings.Add(items[3]);
+                    obsRankings.Add(items[4]);
+                    break;
+            }
+            ranking1.DataContext = obsRankings[0];
+            ranking2.DataContext = obsRankings[1];
+            ranking3.DataContext = obsRankings[2];
+            ranking4.DataContext = obsRankings[3];
+            ranking5.DataContext = obsRankings[4];
         }
     }
 }
---
+++ AuditPracticalOperation/ViewModel/RankingItem.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ViewModel
+{
+    public class RankingItem : ViewModelBase
+    {
+        private string imageSource;
+        private string user_name;
+        private int score;
+        public string ImgSource
+        {
+            get
+            {
+                return imageSource;
+            }
+            set
+            {
+                if (value != imageSource)
+                {
+                    imageSource = value;
+                    RaisePropertyChanged("ImgSource");
+                }
+            }
+        }
+        public string User_Name
+        {
+            get
+            {
+                return user_name;
+            }
+            set
+            {
+                if (user_name != value)
+                {
+                    user_name = value;
+                    RaisePropertyChanged("User_Name");
+                }
+            }
+        }
+        public int Score
+        {
+            get
+            {
+                return score;
+            }
+            set
+            {
+                if (score != value)
+                {
+                    score = value;
+                    RaisePropertyChanged("Score");
+                }
+            }
+        }
+    }
+
+}
